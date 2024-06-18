1c6a7b73a149de5a5eec75ddfc20f14c732e0dc0
josh <joshiszatt@gmail.com>
2024-06-18 12:57:49 +0800

    Update logging and error handling in pipeline code
    Improved the logging system in the pipeline code for better debugging and granular control. Errors now correctly report which file type was rejected. The count and name of the current file in batch execution are now also logged to aid in progress tracking.

--- phunky/pipelines.py
+++ phunky/pipelines.py
@@ -17,7 +17,7 @@ from .functions import (
 # _____________________________________________________PIPELINES
 
 
-def assembly_pipeline(input_file, output_dir, isolate='unknown',
+def assembly_pipeline(input_file, output_dir, isolate='phage',
                       logger=None, logfile_location=None, logfile_configuration=None):
     # Setting logger if None has been passed
     if logger is None:
@@ -34,9 +34,9 @@ def assembly_pipeline(input_file, output_dir, isolate='unknown',
 
     # Check if isolate value is allowed
     if isolate == 'phage':
-        target = 30000000 # Approx 100 X coverage for a 300kb genome
+        target = 30000000  # Approx 100 X coverage for a 300kb genome
     elif isolate == 'bacterial':
-        target = 500000000 # Approx 100 X coverage for a 5mb genome
+        target = 500000000  # Approx 100 X coverage for a 5mb genome
     elif isolate == 'fungal':
         target = 5000000000  # Approx 100 X coverage for a 50mb genome
     elif isolate == 'unknown':
@@ -44,19 +44,25 @@ def assembly_pipeline(input_file, output_dir, isolate='unknown',
     else:
         raise ValueError("Isolate must be: 'phage', 'bacterial', 'fungal' or 'unknown'")
 
+    # Log
+    logger.info(f"Target coverage set to {target}")
+
     # Create output location
     extensions = ['.bam', '.fastq', '.fastq.gz']
     basename = os.path.basename(str(input_file))
+    logger.debug(f'Basename: {basename}')
     name = out = None
     for extension in extensions:
         if basename.endswith(extension):
             name = basename[:-len(extension)]
+            logger.debug(f'Name: {name}')
             out = os.path.join(output_dir, name)
             logger.info(f'File type ({extension}) accepted: output_directory: {out}')
             os.makedirs(out, exist_ok=False)
             break
-        else:
-            raise Exception("File type not accepted")
+    else:
+        logger.error(f"File type rejected: {basename}")
+        raise Exception("File type rejected")
 
     # Ensure variables are set
     if out is None or name is None:
@@ -71,55 +77,64 @@ def assembly_pipeline(input_file, output_dir, isolate='unknown',
         convert_bam_to_fastq(input_file, fq_raw)
     else:
         fq_raw = input_file
+    logger.info(f"Path to raw data: {fq_raw}")
 
     # Remove adapters
+    logger.info("Trimming adaptors from raw reads")
     fq_trim = os.path.join(out, f'{name}_trimmed.fastq')
     porechop_abi(fq_raw, fq_trim)
+    logger.info(f"Path to trimmed data: {fq_trim}")
 
     # Raw QC
     outdir = os.path.join(out, 'nanoplot_raw')
     nanoplot(fq_raw, outdir)
+    logger.info("Raw reads QC complete")
 
     # Trimmed QC
     outdir = os.path.join(out, 'nanoplot_trimmed')
     nanoplot(fq_trim, outdir)
+    logger.info("Trimmed reads QC complete")
 
     # gzip file
     fq_trim_gz = gzip_file(fq_trim)
 
     # Filter
+    logger.info(f"Filtering data to {target}bp")
     fq_filt = os.path.join(out, f'{name}_filtered.fastq')
     filtlong(fq_trim_gz, fq_filt,
              target_bases=target)
+    logger.info(f"Path to filtered data: {fq_filt}")
 
     # Filtered QC
     outdir = os.path.join(out, 'nanoplot_filtered')
     nanoplot(fq_filt, outdir)
+    logger.info("Filtered reads QC complete")
 
     # Genome assembly
+    logger.info("Beginning genome assembly...")
     outdir = os.path.join(out, 'Flye_assembly')
     read_type = None
     contigs = False
 
     if fq_filt:
-        print("Using filtered reads for assembly")
+        logger.info("Using filtered reads for assembly")
         read_type = 'filtered'
         contigs = flye_assembly(fq_filt, outdir, raise_on_fail=False)
 
     if not contigs:
-        print("Filtered assembly failed. Using trimmed reads for assembly")
+        logger.info("Filtered assembly failed. Using trimmed reads for assembly")
         read_type = 'trimmed'
         contigs = flye_assembly(fq_trim, outdir, raise_on_fail=False)
 
     if not contigs:
-        print("Trimmed reads assembly failed. Using raw reads for assembly")
+        logger.info("Trimmed reads assembly failed. Using raw reads for assembly")
         read_type = 'raw'
         contigs = flye_assembly(fq_raw, outdir)
 
     # Read mapping
+    logger.info("Mapping reads to the assembly")
     fa_filt = os.path.join(out, f'{name}_{read_type}.fasta')
     convert_bam_to_fastq(fq_filt, fa_filt)
-
     outdir = os.path.join(out, 'Read_mapping')
     basecov = read_mapping(
         contigs_fasta=contigs,
@@ -128,6 +143,7 @@ def assembly_pipeline(input_file, output_dir, isolate='unknown',
     )[0]
 
     # Using basecov.tsv and header to generate coverage graph
+    logger.info("Generating coverage graph")
     header = extract_contig_header(contigs)[0]
     generate_coverage_graph(
         header=header,
@@ -136,9 +152,12 @@ def assembly_pipeline(input_file, output_dir, isolate='unknown',
 
     # CheckV
     if os.getenv('CHECKVDB'):
+        logger.info(f"Checkv database detected, running analysis: {os.getenv('CHECKVDB')}")
         outdir = os.path.join(out, 'CheckV')
         checkv(contigs, outdir)
 
+    # Finish pipeline
+    logger.info(f"Pipeline complete: {basename}")
 
 # _____________________________________________________BATCHES
 
@@ -174,7 +193,10 @@ def batch_assembly_pipeline(input_dir, output_dir, isolate=None,
     input_dir = os.path.expanduser(input_dir)
     output_dir = os.path.expanduser(output_dir)
     os.makedirs(output_dir, exist_ok=True)
+    count = 0
     for file in os.listdir(input_dir):
+        count = count+1
+        logger.info(f"Running input file #{count}")
         path = os.path.join(input_dir, file)
         try:
             assembly_pipeline(
@@ -186,3 +208,6 @@ def batch_assembly_pipeline(input_dir, output_dir, isolate=None,
         except Exception as e:
             logger.error(f"Pipeline failure ({file}): {e}")
             continue
+
+    # Finish batch pipeline
+    logger.info("Batch pipeline complete")
